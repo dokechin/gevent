c911192f9e6873402b4fbd64b32f70cd9ae6bdfc
Tetsuya Tatsumi <ttatsumi@ra2.so-net.ne.jp>
2013-04-21 07:10:45 +0900

    add specifing config file path, for production env

---
+++ lib/GEvent/Web.pm
@@ -0,0 +1,31 @@
+package GEvent::Web;
+use Mojo::Base 'Mojolicious';
+use GEvent::DB;
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+  $self->plugin('share_helpers');
+
+
+  my $ config = $self->plugin('Config', { file => 'g_event-web.conf' });
+  
+  $self->helper(db => sub { GEvent::DB->new ( $config->{db} )});
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('root#index');
+  $r->post('/newentry')->to('newentry#register');
+  $r->get('/event')->to('event#search');
+  $r->post('/event')->to('event#create');
+  $r->get('/event/:id')->to('event#show');
+  $r->get('/new')->to('new#index');
+
+}
+
+1;
