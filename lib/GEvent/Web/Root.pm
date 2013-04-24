eefbfd5f5f0f3f130a5fa45559f64b77cc4d9212
Tetsuya Tatsumi <ttatsumi@ra2.so-net.ne.jp>
2013-04-24 22:52:56 +0900

    fix tab activate bug

--- lib/GEvent/Web/Root.pm
+++ lib/GEvent/Web/Root.pm
@@ -13,32 +13,4 @@ sub index {
     message => 'Welcome to the Mojolicious real-time web framework!');
 }
 
-sub newentry {
-  my $self = shift;
-
-  my $validator = FormValidator::Lite->new( $self->req );
-  $validator->set_param_message( textinput => '会員番号',);
-
-  $validator->set_message(
-        'textinput.not_null' => '[_1] が空です',
-        'textinput.uint' => '[_1] が数値でありません');
-
-  my $res = $validator->check(
-        textinput => [qw/NOT_NULL UINT/],
-    );
-
-  if($validator->has_error) {
-
-     $self->stash->{error_messages} = $validator->get_error_messages();
-     $self->stash->{errors} = $validator->errors();
-     my $html = $self->render_partial(template => 'example/welcome')->to_string;
-     $self->render_text(
-            HTML::FillInForm->fill(\$html, $self),
-            format => 'html'
-     );
-  }
-   else{
-     $self->render(name => $self->param("textinput"));
-  }
-}
 1;
